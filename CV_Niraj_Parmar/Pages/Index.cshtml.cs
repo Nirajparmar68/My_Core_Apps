697b9ef9f93269de5f9034267a88c8405fd25c50
Niraj Parmar <niraj.parmar68@gmail.com>
2024-12-26 00:21:16 +0530

    Updated CV indec with services, portfolio, skills etc.

--- CV_Niraj_Parmar/Pages/Index.cshtml.cs
+++ CV_Niraj_Parmar/Pages/Index.cshtml.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CV_Niraj_Parmar.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+
+        // Example Date of Birth
+        public DateTime DateOfBirth { get; set; } = new DateTime(1994, 01, 07);
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -15,6 +17,17 @@ namespace CV_Niraj_Parmar.Pages
         public void OnGet()
         {
 
+        }
+
+
+        public int CalculateAge(DateTime dob)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dob.Year;
+
+            if (dob.Date > today.AddYears(-age)) age--; // Adjust if birthday hasn't occurred yet this year
+
+            return age;
         }
     }
 }
